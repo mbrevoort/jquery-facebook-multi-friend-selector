ee8be46f7e71808d7118ddcd5017785f71b04a64
Mike Brevoort <mike@brevoort.com>
2011-01-20 23:26:48 -0700

    updated with my latest diversion with progressive image loading. also added index.html that expects to be accessed at http://localhost:8080/index.html

--- jquery.facebook.multifriend.select.js
+++ jquery.facebook.multifriend.select.js
@@ -16,11 +16,17 @@
    
 (function($) { 
     var JFMFS = function(element, options) {
-        var elem = $(element);
-        var obj = this;
+        var elem = $(element),
+            obj = this,
+            uninitializedImagefriendElements = [], // for images that are initialized
+            keyUpTimer,
+            friend_container = ;
+            
         var settings = $.extend({
             max_selected: -1,
-            max_selected_message: "{0} of {1} selected"
+            max_selected_message: "{0} of {1} selected",
+            init_callback: function() {},
+            onchange_callback: function(selectedIds) {}
         }, options || {});
         var lastSelected;  // used when shift-click is performed to know where to start from to select multiple elements
                 
@@ -31,7 +37,7 @@
         elem.html(
             "<div id='jfmfs-friend-selector'>" +
             "    <div id='jfmfs-inner-header'>" +
-            "        <span class='jfmfs-title'>Find Friends: </span><input type='text' id='jfmfs-friend-filter-text'/>" +
+            "        <span class='jfmfs-title'>Find Friends: </span><input type='text' id='jfmfs-friend-filter-text' value='Start typing a name'/>" +
             "        <a class='filter-link selected' id='jfmfs-filter-all' href='#'>All</a>" +
             "        <a class='filter-link' id='jfmfs-filter-selected' href='#'>Selected (<span id='jfmfs-selected-count'>0</span>)</a>" +
             ((settings.max_selected > 0) ? "<div id='jfmfs-max-selected-wrapper'></div>" : "") +
@@ -40,10 +46,26 @@
             "</div>" 
         );
             
-        FB.api('/me/friends', function(response) {
-            $.each(response.data, function(i, friend) {
-                $("#jfmfs-friend-container").append("<div class='jfmfs-friend' id='" + friend.id  +"'><img src='https://graph.facebook.com/" + friend.id + "/picture' /><div class='friend-name'>" + friend.name + "</div></div>");
+        FB.api('/me/friends?fields=id,name', function(response) {
+            var sortedFriendData = response.data.sort(function(a, b) {
+                var x = a.name.toLowerCase();
+                var y = b.name.toLowerCase();
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
             });
+            
+            
+            
+            $.each(sortedFriendData, function(i, friend) {
+                $("#jfmfs-friend-container").append("<div class='jfmfs-friend' id='" + friend.id  +"'><img/><div class='friend-name'>" + friend.name + "</div></div>");
+            
+                $("#" + friend.id, elem).bind('inview', function (event, visible) {
+                    if( $(this).attr('src') == undefined)
+                        $("img", $(this)).attr("src", "//graph.facebook.com/" + friend.id + "/picture");
+                });
+            
+            });
+            
+            uninitializedImagefriendElements = $(".jfmfs-friend", elem);            
             init();
         });
         
@@ -60,6 +82,18 @@
             return ids;
         };
         
+        this.getSelectedIdsAndNames = function() {
+            var selected = [];
+            $.each(elem.find(".jfmfs-friend.selected"), function(i, friend) {
+                selected.push( {id: $(friend).attr("id"), name: $(friend).find(".friend-name").text()});
+            });
+            return selected
+        };
+        
+        this.clearSelected = function () {
+            $(".jfmfs-friend").removeClass("selected");
+        };
+        
         // ----------+----------+----------+----------+----------+----------+----------+
         // Private functions
         // ----------+----------+----------+----------+----------+----------+----------+
@@ -72,8 +106,13 @@
                 // already been selected, if so, just return
                 if(!$(this).hasClass("selected") && 
                     maxSelectedEnabled() &&
-                    $(".jfmfs-friend.selected").size() >= settings.max_selected)
+                    $(".jfmfs-friend.selected").size() >= settings.max_selected &&
+                    settings.max_selected != 1)
                     return;
+                    
+                // if the max is 1 then unselect the current and select the new    
+                if(settings.max_selected == 1)
+                    elem.find(".selected").removeClass("selected");
                     
                 $(this).toggleClass("selected");
                 $(this).removeClass("hover");
@@ -108,6 +147,8 @@
                 if( maxSelectedEnabled() ) {
                     updateMaxSelectedMessage();
                 }
+                
+                settings.onchange_callback( obj.getSelectedIdsAndNames() );
             });
 
             // filter by selected, hide all non-selected
@@ -139,13 +180,17 @@
             elem.find("#jfmfs-friend-filter-text")
                 .keyup( function() {
                     var filter = $(this).val();
-                    if(filter == '') {
-                        $(".jfmfs-friend").removeClass("hide-filtered");
-                    }
-                    else {
-                        $("#jfmfs-friend-selector").find(".friend-name:not(:Contains(" + filter +"))").parent().addClass("hide-filtered");
-                        $("#jfmfs-friend-selector").find(".friend-name:Contains(" + filter +")").parent().removeClass("hide-filtered");                         
-                    }    
+                    clearTimeout(keyUpTimer);
+                    keyUpTimer = setTimeout( function() {
+                        if(filter == '') {
+                            $(".jfmfs-friend").removeClass("hide-filtered");
+                        }
+                        else {
+                            $("#jfmfs-friend-selector").find(".friend-name:not(:Contains(" + filter +"))").parent().addClass("hide-filtered");
+                            $("#jfmfs-friend-selector").find(".friend-name:Contains(" + filter +")").parent().removeClass("hide-filtered");                         
+                        }    
+                        showImagesInViewPort();                        
+                    }, 400);
                 })
                 .focus( function() {
                     if($.trim($(this).val()) == 'Start typing a name')
@@ -162,8 +207,53 @@
                 function(){ $(this).removeClass("jfmfs-button-hover");}
             );      
             
-            updateMaxSelectedMessage();          
+            // manages lazy loading of images
+            var getViewportHeight = function() {
+                var height = window.innerHeight; // Safari, Opera
+                var mode = document.compatMode;
+
+                if ( (mode || !$.support.boxModel) ) { // IE, Gecko
+                    height = (mode == 'CSS1Compat') ?
+                    document.documentElement.clientHeight : // Standards
+                    document.body.clientHeight; // Quirks
+                }
+
+                return height;
+            };
             
+            var showImagesInViewPort = function() {
+                var vpH = $("#jfmfs-friend-container").innerHeight() ,
+                    scrolltop = $("#jfmfs-friend-container").scrollTop(),
+                    filter = $("#jfmfs-friend-filter-text", elem).val();
+                
+                var foundVisible = false;
+                $.each(uninitializedImagefriendElements, function(i, $el){
+                    if($el != null) {
+                        var $el = $( uninitializedImagefriendElements[i] ),
+                            top = $el.offset().top - $el.parent().offset().top,                        
+                            height = $el.height();
+                        if (top+height >= -10 && top-height < vpH) {  // give some extra padding for broser differences
+                                $el.data('inview', true);
+                                $el.trigger('inview', [ true ]);
+                                foundVisible = true;
+                                uninitializedImagefriendElements[i] = null; 
+                        } 
+                        else {
+                            if(foundVisible) return false;
+                        }                            
+                    }              
+                });
+            };
+
+            $("#jfmfs-friend-container").bind('scroll', function () {
+                showImagesInViewPort();
+            });
+
+            updateMaxSelectedMessage();                      
+            settings.init_callback();
+            // there's a race condition with the colorbox and the images actually
+            // being in the viewport, so just set a timer 
+            setTimeout(showImagesInViewPort, 1000);
         };
 
         var selectedCount = function() {
